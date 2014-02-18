eb2631d43ced9cf8c54cc901977d9b697e0d14e8
Gilagamesh <Genesis@Rainbow.local>
2014-02-19 00:41:33 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UnionStudent::Application.config.secret_key_base = 'cefbdd5174595c53539d279c9167ba02c2b6bf721c4eb068ee2737c44d9e595ca3a355ca9f2645c711a2686008ada7639e815f1d928d9c6571b34e90d9921a16'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+UnionStudent::Application.config.session_store :cookie_store, key: '_union_student_session'
