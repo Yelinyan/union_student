43ae92359f10a7ed407c3f9c3b9eaa9fd0a2e8af
Gilagamesh <Genesis@Rainbow.local>
2014-03-08 10:46:50 +0800

    add some example

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ActiveRecord::Base
+	default_scope -> { order('created_at DESC') }
 	belongs_to :belong
 #	validates :belong, :presence => true
 end
--- app/models/down_load_file.rb
+++ app/models/down_load_file.rb
@@ -1,5 +1,5 @@
-
 class DownLoadFile < ActiveRecord::Base
+    default_scope -> { order('created_at DESC') }
 	mount_uploader :asset, AssetUploader
 #	attr_accessible :asset, :asset_cache, :remove_asset
 end
